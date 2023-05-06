1f3755c3403b3406c510800c3b151ae8d98954c7
Parker <pp101socials@gmail.com>
2023-05-06 11:55:18 -0400

    Created file

---
+++ assets/animations/FADES.js
@@ -0,0 +1,40 @@
+/**
+ *
+ *  This is the fade animations
+ *
+ */
+
+const FADE_IN = {
+  visible: {
+    opacity: 1,
+    transition: { duration: 0.9, delay: 1 },
+  },
+  hidden: {
+    opacity: 0,
+  },
+};
+
+const FADE_LEFT = {
+  visible: {
+    opacity: 1,
+    x: 0,
+    transition: { duration: 0.9 },
+  },
+  hidden: {
+    opacity: 0,
+    x: 50,
+  },
+};
+const FADE_RIGHT = {
+  visible: {
+    opacity: 1,
+    x: 0,
+    transition: { duration: 0.9 },
+  },
+  hidden: {
+    opacity: 0,
+    x: -50,
+  },
+};
+
+export { FADE_IN, FADE_LEFT, FADE_RIGHT };
