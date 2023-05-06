87ce31aabc0c285104f3d22d8fb20c3f04f8f2b0
Parker <pp101socials@gmail.com>
2023-05-06 11:55:11 -0400

    Created file

--- assets/data/variables/OBJECTS.js
+++ assets/data/variables/OBJECTS.js
@@ -1,1 +1,288 @@
 // This file is used to hold some objects
+
+import PortfolioItem from "../classes/PortfolioItem";
+import {
+  BG_BLACKLIGHT_1,
+  BG_BLACKLIGHT_2,
+  BG_BLACKLIGHT_3,
+  BG_BLACKLIGHT_4,
+  BG_BLACKLIGHT_5,
+  BG_BLACKLIGHT_6,
+  BG_BLACKLIGHT_7,
+  BG_BLACKLIGHT_8,
+  BG_BLACKLIGHT_9,
+  BG_BLACKLIGHT_10,
+  BG_BLACKLIGHT_11,
+  BG_BLACKLIGHT_12,
+  BG_BLACKLIGHT_13,
+  BG_BLACKLIGHT_14,
+  BG_BLACKLIGHT_15,
+  BG_BLACKLIGHT_16,
+  BG_BLACKLIGHT_17,
+  BG_BLACKLIGHT_18,
+  BG_BLACKLIGHT_19,
+  BG_BLACKLIGHT_20,
+  BG_PORTRAIT_1,
+  BG_PORTRAIT_2,
+  BG_PORTRAIT_3,
+  BG_PORTRAIT_4,
+  BG_PORTRAIT_5,
+  BG_PORTRAIT_6,
+  BG_PORTRAIT_7,
+  BG_PORTRAIT_8,
+  BG_PORTRAIT_9,
+  BG_PORTRAIT_10,
+  BG_PORTRAIT_11,
+  BG_PORTRAIT_12,
+  BG_PORTRAIT_13,
+  BG_PORTRAIT_14,
+  BG_PORTRAIT_15,
+  BG_PORTRAIT_16,
+  BG_PORTRAIT_17,
+  BG_PORTRAIT_18,
+  BG_PORTRAIT_19,
+  BG_PORTRAIT_20,
+  BG_PORTRAIT_21,
+  BG_PORTRAIT_22,
+  BG_PORTRAIT_23,
+  BG_PORTRAIT_24,
+  BG_PORTRAIT_25,
+  BG_PORTRAIT_26,
+  BG_PORTRAIT_27,
+  BG_PORTRAIT_28,
+  BG_PORTRAIT_29,
+  BG_PORTRAIT_30,
+  BG_PORTRAIT_31,
+  BG_PORTRAIT_32,
+  BG_PORTRAIT_33,
+  BG_PORTRAIT_34,
+  BG_PORTRAIT_35,
+  BG_PORTRAIT_36,
+  BG_PORTRAIT_37,
+  BG_PORTRAIT_38,
+  BG_PORTRAIT_39,
+  BG_PORTRAIT_40,
+  BG_PORTRAIT_41,
+  BG_PORTRAIT_42,
+  BG_PORTRAIT_43,
+  BG_PORTRAIT_44,
+  BG_PORTRAIT_45,
+  BG_PORTRAIT_46,
+  BG_PORTRAIT_47,
+  BG_PORTRAIT_48,
+  BG_PORTRAIT_49,
+  BG_PORTRAIT_50,
+  BG_PORTRAIT_51,
+  BG_PORTRAIT_52,
+  BG_PORTRAIT_53,
+  BG_PORTRAIT_54,
+  BG_PORTRAIT_55,
+  BG_PORTRAIT_56,
+  BG_PORTRAIT_57,
+  BG_PORTRAIT_58,
+  BG_PORTRAIT_59,
+  BG_PORTRAIT_60,
+  BG_PORTRAIT_61,
+  BG_PORTRAIT_62,
+  BG_PROJECTION_1,
+  BG_PROJECTION_2,
+  BG_PROJECTION_3,
+  BG_PROJECTION_4,
+  BG_PROJECTION_5,
+  BG_PROJECTION_6,
+  BG_PROJECTION_7,
+  BG_PROJECTION_8,
+  BG_PROJECTION_9,
+} from "../../cdns/CDNBgs";
+
+const OBJ_BL_1 = new PortfolioItem("Blacklight", BG_BLACKLIGHT_1);
+const OBJ_BL_2 = new PortfolioItem("Blacklight", BG_BLACKLIGHT_2);
+const OBJ_BL_3 = new PortfolioItem("Blacklight", BG_BLACKLIGHT_3);
+const OBJ_BL_4 = new PortfolioItem("Blacklight", BG_BLACKLIGHT_4);
+const OBJ_BL_5 = new PortfolioItem("Blacklight", BG_BLACKLIGHT_5);
+const OBJ_BL_6 = new PortfolioItem("Blacklight", BG_BLACKLIGHT_6);
+const OBJ_BL_7 = new PortfolioItem("Blacklight", BG_BLACKLIGHT_7);
+const OBJ_BL_8 = new PortfolioItem("Blacklight", BG_BLACKLIGHT_8);
+const OBJ_BL_9 = new PortfolioItem("Blacklight", BG_BLACKLIGHT_9);
+const OBJ_BL_10 = new PortfolioItem("Blacklight", BG_BLACKLIGHT_10);
+const OBJ_BL_11 = new PortfolioItem("Blacklight", BG_BLACKLIGHT_11);
+const OBJ_BL_12 = new PortfolioItem("Blacklight", BG_BLACKLIGHT_12);
+const OBJ_BL_13 = new PortfolioItem("Blacklight", BG_BLACKLIGHT_13);
+const OBJ_BL_14 = new PortfolioItem("Blacklight", BG_BLACKLIGHT_14);
+const OBJ_BL_15 = new PortfolioItem("Blacklight", BG_BLACKLIGHT_15);
+const OBJ_BL_16 = new PortfolioItem("Blacklight", BG_BLACKLIGHT_16);
+const OBJ_BL_17 = new PortfolioItem("Blacklight", BG_BLACKLIGHT_17);
+const OBJ_BL_18 = new PortfolioItem("Blacklight", BG_BLACKLIGHT_18);
+const OBJ_BL_19 = new PortfolioItem("Blacklight", BG_BLACKLIGHT_19);
+const OBJ_BL_20 = new PortfolioItem("Blacklight", BG_BLACKLIGHT_20);
+
+const OBJ_PORT_1 = new PortfolioItem("Portrait", BG_PORTRAIT_1);
+const OBJ_PORT_2 = new PortfolioItem("Portrait", BG_PORTRAIT_2);
+const OBJ_PORT_3 = new PortfolioItem("Portrait", BG_PORTRAIT_3);
+const OBJ_PORT_4 = new PortfolioItem("Portrait", BG_PORTRAIT_4);
+const OBJ_PORT_5 = new PortfolioItem("Portrait", BG_PORTRAIT_5);
+const OBJ_PORT_6 = new PortfolioItem("Portrait", BG_PORTRAIT_6);
+const OBJ_PORT_7 = new PortfolioItem("Portrait", BG_PORTRAIT_7);
+const OBJ_PORT_8 = new PortfolioItem("Portrait", BG_PORTRAIT_8);
+const OBJ_PORT_9 = new PortfolioItem("Portrait", BG_PORTRAIT_9);
+const OBJ_PORT_10 = new PortfolioItem("Portrait", BG_PORTRAIT_10);
+const OBJ_PORT_11 = new PortfolioItem("Portrait", BG_PORTRAIT_11);
+const OBJ_PORT_12 = new PortfolioItem("Portrait", BG_PORTRAIT_12);
+const OBJ_PORT_13 = new PortfolioItem("Portrait", BG_PORTRAIT_13);
+const OBJ_PORT_14 = new PortfolioItem("Portrait", BG_PORTRAIT_14);
+const OBJ_PORT_15 = new PortfolioItem("Portrait", BG_PORTRAIT_15);
+const OBJ_PORT_16 = new PortfolioItem("Portrait", BG_PORTRAIT_16);
+const OBJ_PORT_17 = new PortfolioItem("Portrait", BG_PORTRAIT_17);
+const OBJ_PORT_18 = new PortfolioItem("Portrait", BG_PORTRAIT_18);
+const OBJ_PORT_19 = new PortfolioItem("Portrait", BG_PORTRAIT_19);
+const OBJ_PORT_20 = new PortfolioItem("Portrait", BG_PORTRAIT_20);
+const OBJ_PORT_21 = new PortfolioItem("Portrait", BG_PORTRAIT_21);
+const OBJ_PORT_22 = new PortfolioItem("Portrait", BG_PORTRAIT_22);
+const OBJ_PORT_23 = new PortfolioItem("Portrait", BG_PORTRAIT_23);
+const OBJ_PORT_24 = new PortfolioItem("Portrait", BG_PORTRAIT_24);
+const OBJ_PORT_25 = new PortfolioItem("Portrait", BG_PORTRAIT_25);
+const OBJ_PORT_26 = new PortfolioItem("Portrait", BG_PORTRAIT_26);
+const OBJ_PORT_27 = new PortfolioItem("Portrait", BG_PORTRAIT_27);
+const OBJ_PORT_28 = new PortfolioItem("Portrait", BG_PORTRAIT_28);
+const OBJ_PORT_29 = new PortfolioItem("Portrait", BG_PORTRAIT_29);
+const OBJ_PORT_30 = new PortfolioItem("Portrait", BG_PORTRAIT_30);
+const OBJ_PORT_31 = new PortfolioItem("Portrait", BG_PORTRAIT_31);
+const OBJ_PORT_32 = new PortfolioItem("Portrait", BG_PORTRAIT_32);
+const OBJ_PORT_33 = new PortfolioItem("Portrait", BG_PORTRAIT_33);
+const OBJ_PORT_34 = new PortfolioItem("Portrait", BG_PORTRAIT_34);
+const OBJ_PORT_35 = new PortfolioItem("Portrait", BG_PORTRAIT_35);
+const OBJ_PORT_36 = new PortfolioItem("Portrait", BG_PORTRAIT_36);
+const OBJ_PORT_37 = new PortfolioItem("Portrait", BG_PORTRAIT_37);
+const OBJ_PORT_38 = new PortfolioItem("Portrait", BG_PORTRAIT_38);
+const OBJ_PORT_39 = new PortfolioItem("Portrait", BG_PORTRAIT_39);
+const OBJ_PORT_40 = new PortfolioItem("Portrait", BG_PORTRAIT_40);
+const OBJ_PORT_41 = new PortfolioItem("Portrait", BG_PORTRAIT_41);
+const OBJ_PORT_42 = new PortfolioItem("Portrait", BG_PORTRAIT_42);
+const OBJ_PORT_43 = new PortfolioItem("Portrait", BG_PORTRAIT_43);
+const OBJ_PORT_44 = new PortfolioItem("Portrait", BG_PORTRAIT_44);
+const OBJ_PORT_45 = new PortfolioItem("Portrait", BG_PORTRAIT_45);
+const OBJ_PORT_46 = new PortfolioItem("Portrait", BG_PORTRAIT_46);
+const OBJ_PORT_47 = new PortfolioItem("Portrait", BG_PORTRAIT_47);
+const OBJ_PORT_48 = new PortfolioItem("Portrait", BG_PORTRAIT_48);
+const OBJ_PORT_49 = new PortfolioItem("Portrait", BG_PORTRAIT_49);
+const OBJ_PORT_50 = new PortfolioItem("Portrait", BG_PORTRAIT_50);
+const OBJ_PORT_51 = new PortfolioItem("Portrait", BG_PORTRAIT_51);
+const OBJ_PORT_52 = new PortfolioItem("Portrait", BG_PORTRAIT_52);
+const OBJ_PORT_53 = new PortfolioItem("Portrait", BG_PORTRAIT_53);
+const OBJ_PORT_54 = new PortfolioItem("Portrait", BG_PORTRAIT_54);
+const OBJ_PORT_55 = new PortfolioItem("Portrait", BG_PORTRAIT_55);
+const OBJ_PORT_56 = new PortfolioItem("Portrait", BG_PORTRAIT_56);
+const OBJ_PORT_57 = new PortfolioItem("Portrait", BG_PORTRAIT_57);
+const OBJ_PORT_58 = new PortfolioItem("Portrait", BG_PORTRAIT_58);
+const OBJ_PORT_59 = new PortfolioItem("Portrait", BG_PORTRAIT_59);
+const OBJ_PORT_60 = new PortfolioItem("Portrait", BG_PORTRAIT_60);
+const OBJ_PORT_61 = new PortfolioItem("Portrait", BG_PORTRAIT_61);
+const OBJ_PORT_62 = new PortfolioItem("Portrait", BG_PORTRAIT_62);
+
+const OBJ_PRO_1 = new PortfolioItem("Projection", BG_PROJECTION_1);
+const OBJ_PRO_2 = new PortfolioItem("Projection", BG_PROJECTION_2);
+const OBJ_PRO_3 = new PortfolioItem("Projection", BG_PROJECTION_3);
+const OBJ_PRO_4 = new PortfolioItem("Projection", BG_PROJECTION_4);
+const OBJ_PRO_5 = new PortfolioItem("Projection", BG_PROJECTION_5);
+const OBJ_PRO_6 = new PortfolioItem("Projection", BG_PROJECTION_6);
+const OBJ_PRO_7 = new PortfolioItem("Projection", BG_PROJECTION_7);
+const OBJ_PRO_8 = new PortfolioItem("Projection", BG_PROJECTION_8);
+const OBJ_PRO_9 = new PortfolioItem("Projection", BG_PROJECTION_9);
+
+export {
+  OBJ_BL_1,
+  OBJ_BL_10,
+  OBJ_BL_11,
+  OBJ_BL_12,
+  OBJ_BL_13,
+  OBJ_BL_14,
+  OBJ_BL_15,
+  OBJ_BL_16,
+  OBJ_BL_17,
+  OBJ_BL_18,
+  OBJ_BL_19,
+  OBJ_BL_2,
+  OBJ_BL_20,
+  OBJ_BL_3,
+  OBJ_BL_4,
+  OBJ_BL_5,
+  OBJ_BL_6,
+  OBJ_BL_7,
+  OBJ_BL_8,
+  OBJ_BL_9,
+};
+export {
+  OBJ_PORT_1,
+  OBJ_PORT_2,
+  OBJ_PORT_3,
+  OBJ_PORT_4,
+  OBJ_PORT_5,
+  OBJ_PORT_6,
+  OBJ_PORT_7,
+  OBJ_PORT_8,
+  OBJ_PORT_9,
+  OBJ_PORT_10,
+  OBJ_PORT_11,
+  OBJ_PORT_12,
+  OBJ_PORT_13,
+  OBJ_PORT_14,
+  OBJ_PORT_15,
+  OBJ_PORT_16,
+  OBJ_PORT_17,
+  OBJ_PORT_18,
+  OBJ_PORT_19,
+  OBJ_PORT_20,
+  OBJ_PORT_21,
+  OBJ_PORT_22,
+  OBJ_PORT_23,
+  OBJ_PORT_24,
+  OBJ_PORT_25,
+  OBJ_PORT_26,
+  OBJ_PORT_27,
+  OBJ_PORT_28,
+  OBJ_PORT_29,
+  OBJ_PORT_30,
+  OBJ_PORT_31,
+  OBJ_PORT_32,
+  OBJ_PORT_33,
+  OBJ_PORT_34,
+  OBJ_PORT_35,
+  OBJ_PORT_36,
+  OBJ_PORT_37,
+  OBJ_PORT_38,
+  OBJ_PORT_39,
+  OBJ_PORT_40,
+  OBJ_PORT_41,
+  OBJ_PORT_42,
+  OBJ_PORT_43,
+  OBJ_PORT_44,
+  OBJ_PORT_45,
+  OBJ_PORT_46,
+  OBJ_PORT_47,
+  OBJ_PORT_48,
+  OBJ_PORT_49,
+  OBJ_PORT_50,
+  OBJ_PORT_51,
+  OBJ_PORT_52,
+  OBJ_PORT_53,
+  OBJ_PORT_54,
+  OBJ_PORT_55,
+  OBJ_PORT_56,
+  OBJ_PORT_57,
+  OBJ_PORT_58,
+  OBJ_PORT_59,
+  OBJ_PORT_60,
+  OBJ_PORT_61,
+  OBJ_PORT_62,
+};
+export {
+  OBJ_PRO_1,
+  OBJ_PRO_2,
+  OBJ_PRO_3,
+  OBJ_PRO_4,
+  OBJ_PRO_5,
+  OBJ_PRO_6,
+  OBJ_PRO_7,
+  OBJ_PRO_8,
+  OBJ_PRO_9,
+};
