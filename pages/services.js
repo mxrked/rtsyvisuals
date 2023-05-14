0fc90051972468753f47a942e8c9c87d337fe544
Parker <pp101socials@gmail.com>
2023-05-14 11:40:27 -0400

    Added components and uEs

--- pages/services.js
+++ pages/services.js
@@ -17,6 +17,8 @@ import { Search } from "@/assets/components/global/Nav/Both/Search";
 import { Footer } from "@/assets/components/global/Footer/Footer";
 
 import { PageTop } from "@/assets/components/pages/All/PageTop";
+import { ServicesTop } from "@/assets/components/pages/Services/ServicesTop";
+import { ServicesMain } from "@/assets/components/pages/Services/ServicesMain";
 
 // Style Imports
 import "../assets/styles/modules/Services/Services.module.css";
@@ -34,6 +36,49 @@ export default function Services() {
     ManipPageLink("disable", "multiple", ".services-link");
   }, []);
 
+  // Scrolling and opening service based on hash
+  useEffect(() => {
+    setTimeout(() => {
+      const HASH = router.asPath.split("#")[1] || "";
+
+      if (HASH === "portraits") {
+        document.querySelectorAll(".services-checkbox")[0].scrollIntoView();
+
+        document.querySelectorAll(".services-checkbox")[0].checked = true;
+        document.querySelectorAll(".services-text")[0].style.height = "auto";
+        document.querySelectorAll(".service-caret")[0].style.transform =
+          "rotate(180deg)";
+      }
+
+      if (HASH === "boudoirs") {
+        document.querySelectorAll(".services-checkbox")[1].scrollIntoView();
+
+        document.querySelectorAll(".services-checkbox")[1].checked = true;
+        document.querySelectorAll(".services-text")[1].style.height = "auto";
+        document.querySelectorAll(".service-caret")[1].style.transform =
+          "rotate(180deg)";
+      }
+
+      if (HASH === "blacklights") {
+        document.querySelectorAll(".services-checkbox")[2].scrollIntoView();
+
+        document.querySelectorAll(".services-checkbox")[2].checked = true;
+        document.querySelectorAll(".services-text")[2].style.height = "auto";
+        document.querySelectorAll(".service-caret")[2].style.transform =
+          "rotate(180deg)";
+      }
+
+      if (HASH === "packages") {
+        document.querySelectorAll(".services-checkbox")[3].scrollIntoView();
+
+        document.querySelectorAll(".services-checkbox")[3].checked = true;
+        document.querySelectorAll(".services-text")[3].style.height = "auto";
+        document.querySelectorAll(".service-caret")[3].style.transform =
+          "rotate(180deg)";
+      }
+    }, 1200);
+  }, []);
+
   return (
     <div id="PAGE" className="page overrides_Services">
       <PageHead />
@@ -45,6 +90,8 @@ export default function Services() {
 
       <main id="PAGE_CNT" className="page-cnt">
         <PageTop pageTopH1="My Services." />
+        <ServicesTop />
+        <ServicesMain />
       </main>
 
       <Footer />
